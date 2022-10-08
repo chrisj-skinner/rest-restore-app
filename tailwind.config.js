5f493daebbf59b998af24409d3fcb28578acda62
Chris Skinner <chris.skinner@domesticandgeneral.com>
2022-10-08 17:53:20 +0100

    build: add storybook config and base setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica Neue, Helvetica, Arial, sans-serif, Sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
