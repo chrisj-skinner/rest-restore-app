5f926cc2a52edcbbf370f53c35f65fa7ce5dcbb7
Chris Skinner <chris.skinner@domesticandgeneral.com>
2022-11-06 15:20:36 +0000

    feat(components): add treatments story and assets

---
+++ src/stories/treatments.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import FacialsPage from '../pages/facials';
+
+export default {
+  title: 'Pages/Treatments',
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const Facials = () => <FacialsPage />;
