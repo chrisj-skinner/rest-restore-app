1eb0bdc50a3acb75ed9e2bb4b92daa8b562f8e13
Chris Skinner <c.skinner1@me.com>
2023-11-05 18:26:20 +0000

    Updates

--- component---src-pages-404-jsx-76a258537c7cc7d39fdd.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrest_restore_app=self.webpackChunkrest_restore_app||[]).push([[256],{7291:function(e,t,n){n.r(t),n.d(t,{Head:function(){return s}});var r=n(7294),a=n(4532);t.default=function(){return r.createElement(a.Z,null,r.createElement("div",{className:"text-sm text-center"},"404 - opps something went wrong"))};var s=function(){return r.createElement(r.Fragment,null,r.createElement("title",null,"404 | Rest and Restore Beauty Treatments – Abbots Langley & Watford"))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-76a258537c7cc7d39fdd.js.map
